f0291f3cabb669d944d6358760b2f07b83f8dc53
evanKim1999 <zone227@naver.com>
2025-07-25 20:37:43 +0900

    [#44] 프로필 수정뷰 레이아웃 수정

--- Pindora/Source/Presentation/Profile/EditProfile/ProfileEditView.swift
+++ Pindora/Source/Presentation/Profile/EditProfile/ProfileEditView.swift
@@ -15,31 +15,8 @@ final class ProfileEditView: UIView {
     let navigationBarView = CustomNavBarView(title: "프로필 수정")
     private let contentView = UIView()
 
-    private let imageLabel: UILabel = {
-        let label = UILabel()
-        label.text = "대표 이미지"
-        label.font = .systemFont(ofSize: 17)
-        label.textColor = .black
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private let personaTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "페르소나"
-        label.font = .systemFont(ofSize: 17)
-        label.textColor = .black
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private let previewLabel: UILabel = {
-        let label = UILabel()
-        label.text = "미리보기"
-        label.textColor = .black
-        label.font = .boldSystemFont(ofSize: 18)
-        return label
-    }()
+    private let imageTitleLabel = UILabel.makeTitleLabel(text: "대표 이미지")
+    private let personaTitleLabel = UILabel.makeTitleLabel(text: "페르소나")
     
     let registerButton: UIButton = {
         let button = UIButton(type: .system)
@@ -49,6 +26,54 @@ final class ProfileEditView: UIView {
         button.layer.cornerRadius = 12
         button.heightAnchor.constraint(equalToConstant: 50).isActive = true
         return button
+    }()
+    
+    private lazy var titleInfoStackView: UIStackView = {
+        let infoImage = UIImageView(image: UIImage(named: "infoImage"))
+        infoImage.contentMode = .scaleAspectFit
+        infoImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            infoImage.widthAnchor.constraint(equalToConstant: 15),
+            infoImage.heightAnchor.constraint(equalToConstant: 15)
+        ])
+        let imageInfoLabel = UILabel.makeSubInfoLabel(text: "Memoji는 직접 생성 해야합니다")
+        let stackView = UIStackView(arrangedSubviews: [infoImage, imageInfoLabel])
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    private lazy var personaInfoStackView: UIStackView = {
+        let image = UIImageView(image: UIImage(named: "infoImage"))
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            image.widthAnchor.constraint(equalToConstant: 15),
+            image.heightAnchor.constraint(equalToConstant: 15)
+        ])
+        let personaInfoLabel = UILabel.makeSubInfoLabel(text: "저장한 장소 및 좋아하는 장소를 통해 나만의 페르소나를 생성")
+        let stackView = UIStackView(arrangedSubviews: [image, personaInfoLabel])
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    private lazy var previewStackView: UIStackView = {
+        let image = UIImageView(image: UIImage(named: "미리보기"))
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            image.widthAnchor.constraint(equalToConstant: 30),
+            image.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        let previewLabel = UILabel.makeTitleLabel(text: "미리보기")
+        let stackView = UIStackView(arrangedSubviews: [image, previewLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 14
+        return stackView
     }()
     
     // MARK: - Initializer
@@ -70,7 +95,7 @@ final class ProfileEditView: UIView {
         
         addSubview(contentView)
         contentView.translatesAutoresizingMaskIntoConstraints = false
-        [imageLabel, imageSelectorView, personaTitleLabel, personaInfoCardView, previewLabel, personaPreviewView, registerButton]
+        [imageTitleLabel,titleInfoStackView, imageSelectorView, personaTitleLabel, personaInfoStackView, personaInfoCardView, previewStackView, personaPreviewView, registerButton]
             .forEach { contentView.addSubview($0); $0.translatesAutoresizingMaskIntoConstraints = false }
 
     }
@@ -89,29 +114,33 @@ final class ProfileEditView: UIView {
             contentView.bottomAnchor.constraint(equalTo: bottomAnchor),
             contentView.widthAnchor.constraint(equalTo: widthAnchor),
             
-            imageLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
-            imageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            imageTitleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 23),
+            imageTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            titleInfoStackView.topAnchor.constraint(equalTo: imageTitleLabel.bottomAnchor,constant: 8),
+            titleInfoStackView.leadingAnchor.constraint(equalTo: imageTitleLabel.leadingAnchor),
             
-            imageSelectorView.topAnchor.constraint(equalTo: imageLabel.bottomAnchor, constant: 20),
-            imageSelectorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
-            imageSelectorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            imageSelectorView.heightAnchor.constraint(equalToConstant: 120),
+            imageSelectorView.topAnchor.constraint(equalTo: titleInfoStackView.bottomAnchor, constant: 10),
+            imageSelectorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 13),
+            imageSelectorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -13),
+            imageSelectorView.heightAnchor.constraint(equalToConstant: 160),
 
-            personaTitleLabel.topAnchor.constraint(equalTo: imageSelectorView.bottomAnchor, constant: 30),
+            personaTitleLabel.topAnchor.constraint(equalTo: imageSelectorView.bottomAnchor, constant: 20),
             personaTitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            personaInfoStackView.topAnchor.constraint(equalTo: personaTitleLabel.bottomAnchor,constant: 8),
+            personaInfoStackView.leadingAnchor.constraint(equalTo: personaTitleLabel.leadingAnchor),
             
-            personaInfoCardView.topAnchor.constraint(equalTo: personaTitleLabel.bottomAnchor, constant: 10),
-            personaInfoCardView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
-            personaInfoCardView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            personaInfoCardView.heightAnchor.constraint(equalToConstant: 120),
+            personaInfoCardView.topAnchor.constraint(equalTo: personaInfoStackView.bottomAnchor, constant: 10),
+            personaInfoCardView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 13),
+            personaInfoCardView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -13),
+            personaInfoCardView.heightAnchor.constraint(equalToConstant: 140),
             
-            previewLabel.topAnchor.constraint(equalTo: personaInfoCardView.bottomAnchor, constant: 30),
-            previewLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            previewStackView.topAnchor.constraint(equalTo: personaInfoCardView.bottomAnchor, constant: 14),
+            previewStackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            previewStackView.widthAnchor.constraint(equalToConstant: 100),
             
-            personaPreviewView.topAnchor.constraint(equalTo: previewLabel.bottomAnchor, constant: 15),
+            personaPreviewView.topAnchor.constraint(equalTo: previewStackView.bottomAnchor, constant: 27),
             personaPreviewView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             personaPreviewView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            personaPreviewView.heightAnchor.constraint(equalToConstant: 80),
             
             registerButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             registerButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
