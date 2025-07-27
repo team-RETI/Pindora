b792b40304bf2ae10909d9986e4ac2c2fc7a34d1
TripleJ709 <ijn9975@naver.com>
2025-07-27 18:51:40 +0900

    [#49] 문서화 주석 추가

--- Pindora/Source/Domain/Repository/DatabaseRepository.swift
+++ Pindora/Source/Domain/Repository/DatabaseRepository.swift
@@ -8,9 +8,38 @@
 import Foundation
 import Combine
 
+/// Firestore 데이터베이스와의 기본 CRUD 작업을 수행하는 레포지토리 프로토콜입니다.
+/// 제네릭 Codable 객체를 사용하여 생성, 조회, 업데이트, 삭제 기능을 제공합니다.
 public protocol DatabaseRepositoryProtocol {
+    
+    /// Firestore에 객체를 생성(저장)합니다.
+    /// - Parameters:
+    ///   - object: Firestore에 저장할 Codable 객체입니다.
+    ///   - collection: 저장할 컬렉션의 이름입니다.
+    ///   - id: 생성할 문서의 고유 ID입니다.
+    /// - Returns: 작업의 성공 여부를 방출하는 AnyPublisher<Void, Error>입니다.
     func create<T: Codable>(_ object: T, at collection: String, id: String) -> AnyPublisher<Void, Error>
+    
+    /// Firestore에서 특정 문서를 조회하여 객체로 반환합니다.
+    /// - Parameters:
+    ///   - collection: 조회할 컬렉션의 이름입니다.
+    ///   - id: 조회할 문서의 ID입니다.
+    ///   - type: 반환받을 객체의 타입입니다.
+    /// - Returns: 디코딩된 객체를 방출하는  AnyPublisher<T, Error>입니다.
     func fetch<T: Codable>(from collection: String, id: String, as type: T.Type) -> AnyPublisher<T, Error>
+    
+    /// Firestore의 특정 문서를 업데이트합니다.
+    /// - Parameters:
+    ///   - object: Firestore에 저장할 Codable 객체입니다.
+    ///   - collection: 업데이트할 컬렉션의 이름입니다.
+    ///   - id: 업데이트할 문서의 ID입니다.
+    /// - Returns: 작업의 성공 여부를 방출하는 AnyPublisher<Void, Error>입니다.
     func update<T: Codable>(_ object: T, at collection: String, id: String) -> AnyPublisher<Void, Error>
+    
+    /// Firestore에서 특정 문서를 삭제합니다.
+    /// - Parameters:
+    ///   - collection: 삭제할 컬렉션의 이름입니다.
+    ///   - id: 삭제할 문서의 ID입니다.
+    /// - Returns: 작업의 성공 여부를 방출하는 AnyPublisher<Void, Error>입니다.
     func delete(from collection: String, id: String) -> AnyPublisher<Void, Error>
 }
--- Pindora/Source/Domain/UseCase/ImageUseCase.swift
+++ Pindora/Source/Domain/UseCase/ImageUseCase.swift
@@ -8,7 +8,20 @@
 import UIKit
 import Combine
 
+/// 이미지 업로드 및 다운로드를 처리하는 유스케이스 프로토콜입니다.
+/// Firebase Storage를 통해 이미지를 업로드하거나 다운로드합니다.
 protocol ImageUsecaseProtocol {
+    
+    /// 이미지를 지정한 폴더에 업로드하고 해당 이미지의 다운로드 URL을 반환합니다.
+    /// - Parameters:
+    ///   - image: 업로드할 `UIImage`입니다.
+    ///   - folder: 저장할 Firebase Storage 폴더 이름입니다.
+    ///   - fileName: 저장할 이미지의 파일 이름입니다.
+    /// - Returns: 이미지의 다운로드 URL을 방출하는 AnyPublisher<URL, Error>입니다.
     func upload(image: UIImage, folder: String, fileName: String) -> AnyPublisher<URL, Error>
+    
+    /// Firebase Storage에서 지정한 경로의 이미지를 다운로드합니다.
+    /// - Parameter path: 다운로드할 이미지의 Firebase Storage 경로입니다.
+    /// - Returns: 다운로드된 `UIImage`를 방출하는 AnyPublisher<UIImage, Error>입니다.
     func download(from path: String) -> AnyPublisher<UIImage, Error>
 }
